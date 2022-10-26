55f66a17abfb3346e26b0322bef568b01dfa16df
leccrj <leccrj@gmail.com>
2022-10-25 23:26:36 -0300

    CRUD e Exceptions

--- src/main/java/br/com/luiz/controllers/SetorController.java
+++ src/main/java/br/com/luiz/controllers/SetorController.java
@@ -2,11 +2,15 @@ package br.com.luiz.controllers;
 
 import java.util.List;
 
+import javax.print.attribute.standard.Media;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.luiz.model.Setor;
 import br.com.luiz.services.SetorService;
 
@@ -17,9 +21,46 @@ public class SetorController {
 	@Autowired
 	private SetorService service;
 	
+	
 	@RequestMapping(method = RequestMethod.GET,
 			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-	public List<Setor> findAll(){
-		return service.findAll();
+	public List<Setor> findAll()
+	{
+		return service.encontrarTodos();
+	}
+	
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.GET,
+			produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public Setor findById(@PathVariable(value = "id") Long id) 
+	{
+		return service.encontrarPorId(id);	
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Setor create(@RequestBody Setor setor) 
+	{
+		return service.criar(setor);
+	}
+	
+
+	@RequestMapping(method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Setor update(@RequestBody Setor setor) 
+	{
+		return service.atualizar(setor);
+	}
+	
+
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") Long id) 
+	{
+		service.apagar(id);	
 	}
 }
--- src/main/java/br/com/luiz/services/SetorService.java
+++ src/main/java/br/com/luiz/services/SetorService.java
@@ -1,37 +1,65 @@
 package br.com.luiz.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.luiz.exceptions.ResourceNotFoundException;
 import br.com.luiz.model.Setor;
+import br.com.luiz.repositories.SetorRepository;
 
 @Service
-public class SetorService {
-
+public class SetorService 
+{
 	private Logger log = Logger.getLogger(SetorService.class.getName());
 	
-	public List<Setor> findAll(){
-		
-		log.info("Retornando todos os setores");
-		
-		List<Setor> setores = new ArrayList<>();
-		for (int i = 0; i < 8; i++) {
-			Setor setor = mockSetor(i);
-			setores.add(setor);
-		}
-		return setores;
-		
+	//injeção de dependencia do repositorio
+	@Autowired
+	SetorRepository repo;
+	
+	
+	public List<Setor> encontrarTodos()
+	{	
+		log.info("Retornando todos os setores");	
+		return repo.findAll();
 	}
 	
-	private Setor mockSetor(int i) {
-		Setor setor = new Setor();
-		setor.setId(0L);
-		setor.setDescricao("setor 1");
-		setor.setDatacriacao(null);
-		setor.setInativo(true);
-		return setor;		
+	
+	public Setor encontrarPorId(Long id) 
+	{
+		log.info("Encontrando por id.");
+		return repo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID!"));		
+	}
+	
+	
+	public Setor criar(Setor st) 
+	{
+		log.info("Criando um setor");
+		return repo.save(st);
+	}
+	
+	
+	public Setor atualizar(Setor st) 
+	{
+		log.info("Atualizando uma pessoa");
+		var se = repo.findById(st.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID!"));
+		
+		se.setDescricao(st.getDescricao());
+		se.setDatacriacao(st.getDatacriacao());
+		se.setInativo(st.getInativo());
+		
+		return repo.save(st);		
+	}
+	
+	
+	public void apagar(Long id) 
+	{
+		log.info("Apagando um setor");
+		var se = repo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID!"));
+		repo.delete(se);
 	}
 }
